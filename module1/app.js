e983a3dc0b789eb1b56b1dbe621ba5ee5dde54fa
giuseppelepew <giuseppelepew@gmail.com>
2016-10-03 00:56:02 +0300

    New files

---
+++ module1/app.js
@@ -0,0 +1,35 @@
+( function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LSController);
+
+LSController.$inject = ['$scope'];
+
+function LSController($scope){
+  $scope.displayStatus = function() {
+    var items = calculateItems($scope.lunch)
+    if (items > 3) {
+      $scope.name = "Too much!";
+    } else if (items > 0) {
+      $scope.name = "Enjoy!";
+    } else {
+      $scope.name = "Please enter data first";
+    }
+  }
+
+  function calculateItems(string) {
+    var lunchList = string.split(',');
+    var nonEmptyItems = 0;
+    alert(nonEmptyItems);
+    for (var i = 0; i < lunchList.length; i++) {
+      if (lunchList[i].trim()) {
+        nonEmptyItems++;
+      }
+    }
+    return nonEmptyItems;
+  }
+}
+
+
+})();
